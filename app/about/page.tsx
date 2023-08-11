d68a30be7f02ff27036e2dd629e569239703813a
Maximilian Graf <maxgraf27@gmail.com>
2023-08-11 15:04:18 +0200

    updated Navbar

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default async function About() {
+    
+    return (
+        <main>
+            <h1>About</h1>
+            <p>This is the about page of UhlOffice</p>
+        </main>
+    )
+}
